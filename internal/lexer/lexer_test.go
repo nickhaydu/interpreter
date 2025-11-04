95911c27fbf9c756a946bdfc918496ffd2bc16e8
nickhaydu <nickhaydu2002@gmail.com>
2025-11-03 22:01:52 -0400

    added  lexer functions that progress it through an input string through the nexttoken method now allowing it pass a basic test of tokens

--- internal/lexer/lexer_test.go
+++ internal/lexer/lexer_test.go
@@ -1,9 +1,8 @@
 package lexer
 
 import (
+	"interpreter/internal/token"
 	"testing"
-
-	"internal/token"
 )
 
 func TestNextToken(t *testing.T) {
@@ -12,5 +11,29 @@ func TestNextToken(t *testing.T) {
 	test := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
-	}{}
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+	l := New(input)
+
+	for i, tt := range test {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d]- tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d]- literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+
 }
